4ef862cd5e5ce3f1c4cb6ca84af441e5a6b76b6c
datlt <datlt@yopaz.vn>
2023-04-22 18:08:36 +0700

    update firebase login and signin

--- app/src/main/java/com/example/app_my_diary/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/app_my_diary/ui/login/LoginFragment.kt
@@ -4,6 +4,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.app_my_diary.R
 import com.example.app_my_diary.base.BaseViewModelFragment
 import com.example.app_my_diary.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : BaseViewModelFragment<FragmentLoginBinding>() {
     override fun initViewModel() {
@@ -15,10 +16,33 @@ class LoginFragment : BaseViewModelFragment<FragmentLoginBinding>() {
             findNavController().navigate(R.id.signUpFragment)
         }
         binding?.textViewLogin?.setOnClickListener {
-            findNavController().navigate(R.id.homeFragment)
+            login()
         }
     }
 
     override fun getLayout(): Int = R.layout.fragment_login
 
+
+    private fun login() {
+        val auth = FirebaseAuth.getInstance()
+        val email = binding?.etEmail?.text.toString().trim()
+        val password = binding?.etPassword?.text.toString().trim()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+//                    Log.d(TAG, "signInWithEmail:success")
+//                    val user = auth.currentUser
+//                    updateUI(user)
+                    findNavController().navigate(R.id.homeFragment)
+                } else {
+                    // If sign in fails, display a message to the user.
+//                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+//                    Toast.makeText(
+//                        baseContext, "Authentication failed.", Toast.LENGTH_SHORT
+//                    ).show()
+//                    updateUI(null)
+                }
+            }
+    }
 }
--- app/src/main/java/com/example/app_my_diary/ui/sign_up/SignUpFragment.kt
+++ app/src/main/java/com/example/app_my_diary/ui/sign_up/SignUpFragment.kt
@@ -4,6 +4,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.app_my_diary.R
 import com.example.app_my_diary.base.BaseViewModelFragment
 import com.example.app_my_diary.databinding.FragmentSignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpFragment : BaseViewModelFragment<FragmentSignupBinding>() {
     override fun initViewModel() {
@@ -11,9 +12,36 @@ class SignUpFragment : BaseViewModelFragment<FragmentSignupBinding>() {
 
     override fun initView() {
         binding?.textViewLogin?.setOnClickListener {
-            findNavController().navigate(R.id.loginFragment  )
+            findNavController().navigate(R.id.loginFragment)
+        }
+        binding?.textViewSignup?.setOnClickListener {
+            signUp()
         }
     }
 
     override fun getLayout(): Int = R.layout.fragment_signup
+
+    private fun signUp() {
+        val auth = FirebaseAuth.getInstance()
+        val email = binding?.etEmail?.text.toString().trim()
+        val password = binding?.etPassword?.text.toString().trim()
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+//                    // Sign in success, update UI with the signed-in user's information
+//                    Log.d(TAG, "createUserWithEmail:success")
+//                    val user = auth.currentUser
+//                    updateUI(user)
+                    findNavController().navigate(R.id.loginFragment)
+                } else {
+                    // If sign in fails, display a message to the user.
+//                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+//                    Toast.makeText(
+//                        baseContext, "Authentication failed.", Toast.LENGTH_SHORT
+//                    ).show()
+//                    updateUI(null)
+                    findNavController().navigate(R.id.homeFragment)
+                }
+            }
+    }
 }
