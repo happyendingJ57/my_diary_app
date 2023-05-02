14e7df374f4894e3d76a3cec087e7432fddee8aa
datlt <datlt@yopaz.vn>
2023-05-02 18:09:49 +0700

    add splash screen

---
+++ app/src/main/java/com/example/app_my_diary/ui/Splash/SplashFragment.kt
@@ -0,0 +1,20 @@
+package com.example.app_my_diary.ui.Splash
+
+import androidx.navigation.fragment.findNavController
+import com.example.app_my_diary.R
+import com.example.app_my_diary.base.BaseViewModelFragment
+import com.example.app_my_diary.databinding.FragmentSplashBinding
+
+class SplashFragment : BaseViewModelFragment<FragmentSplashBinding>() {
+    override fun initViewModel() {
+
+    }
+
+    override fun initView() {
+        binding?.tvTitle?.postDelayed({
+            findNavController().navigate(R.id.loginFragment)
+        }, 1000)
+    }
+
+    override fun getLayout(): Int = R.layout.fragment_splash
+}
